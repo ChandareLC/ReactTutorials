fab0a10acff502ff0e11fb57ee67a6a74d098aea
chathuranga-chandrarathna <chathuranga.chandrarathna@kogntiv.com>
2023-03-21 09:53:46 +0530

    Create Function and Class Component

---
+++ my-app/src/Component/Button/button.js
@@ -0,0 +1,9 @@
+const Button = () => {
+    return(
+      <div className='my-2'>
+          <button className="bg-red-400 text-white px-5 py-3 rounded-full">Chathuranga</button>
+      </div>
+    );
+}
+
+export default  Button;
