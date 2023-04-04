e434f3d3efdb1cc2026e1395f319c0733622fe72
chathuranga-chandrarathna <chathuranga.chandrarathna@kogntiv.com>
2023-04-04 18:26:40 +0530

    Not using the composition way

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,15 +5,24 @@ import HelloWorld from "./Component/HelloWorld/helloWorld";
 import Post from "./Component/Post/post";
 import Sidebar from "./Component/SideBar/sidebar";
 import ButtonContext from "./Component/Context/buttonContext";
+import UserContext from "./Component/Context/UserContext";
 
 
 function App() {
+    let userData = {
+        name: 'Chathuranga',
+        greet: function (){
+            return 'Hello ' + this.name
+        }
+    }
   return (
     <div className="container mx-auto">
         <div className='flex'>
             <div className='w-1/5'>
            <ButtonContext.Provider value='Chathuranga Lakshan Context'>
+               <UserContext.Provider value={userData}>
                 <Sidebar />
+               </UserContext.Provider>
            </ButtonContext.Provider>
             </div>
        <div className='w-4/5'>
